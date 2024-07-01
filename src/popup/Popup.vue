0e68d005e4000a57c804f973602cf7952f161103
baclt <baclt1580@gmail.com>
2024-07-02 00:15:23 +0800

    2024-7-2

--- src/popup/Popup.vue
+++ src/popup/Popup.vue
@@ -1,17 +1,23 @@
 <script setup lang="ts">
 import { onMessage, sendMessage } from 'webext-bridge/popup';
 import YellowButton from './components/button/YellowButton/YellowButton.vue';
-function toLogin() {
-  console.log("发送message")
-  sendMessage("toLogin", "", "content-script")
+import Notification from './components/Notification/Notification.vue';
+async function toLogin() {
+  let tabs = await browser.tabs.query({ active: true, currentWindow: true })
+  const activeTab = tabs[0];
+  console.log("activeTab",activeTab)
+  sendMessage("toLogin", "", `content-script@${activeTab.id}`)
 }
 </script>
 <template>
-  <div class="w-[300px] h-[200px] bg-white p-2">
-    <section>
+  <div class="w-[300px] h-[400px] bg-white p-2">
+    <section class="flex justify-between">
       <YellowButton @click="toLogin">
         <span class="text-sm">登录</span>
       </YellowButton>
+      <Notification class="ml-auto size-8 mr-2 cursor-pointer"></Notification>
+      <img src="./assets/img/setting.svg" alt="进入设置" class="w-8 cursor-pointer">
     </section>
+    
   </div>
 </template>
---
+++ src/popup/components/Notification/Notification.vue
@@ -0,0 +1,27 @@
+<script lang='ts' setup>
+
+</script>
+<template>
+  <div class="notification transition-transform duration-75">
+    <img src="./assets/img/notification.svg" class="w-full" alt="通知">
+  </div>
+
+</template>
+
+<script lang='ts'>
+
+
+export default {
+
+}
+</script>
+
+<style scoped>
+.notification:hover {
+  transform: scale(1.1)
+  
+ 
+}
+
+
+</style>
