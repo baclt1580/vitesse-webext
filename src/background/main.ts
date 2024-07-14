c54453a8914a70a0b68818f36f4c32bac263a46a
baclt <baclt1580@gmail.com>
2024-07-15 00:34:36 +0800

    2024-7-15

--- src/background/main.ts
+++ src/background/main.ts
@@ -26,13 +26,34 @@ if (import.meta.hot) {
 // })
 
 browser.runtime.onInstalled.addListener(async () => {
+  await clearPageStorage()
   initContainer()
-  let {init:initActiveTabId}=useActiveTabId()
+  let { init: initActiveTabId } = useActiveTabId()
   initActiveTabId()
+
   translateController()
   controller()
 })
-
+//清理所有的页面级缓存
+async function clearPageStorage() {
+  let items = await browser.storage.local.get(null);
+  if (chrome.runtime.lastError) {
+    console.error("获取数据时出错:", chrome.runtime.lastError);
+    return;
+  }
+  const keysToRemove = Object.keys(items).filter(key => key.endsWith('_page'));
+  if (keysToRemove.length > 0) {
+    chrome.storage.local.remove(keysToRemove, () => {
+      if (chrome.runtime.lastError) {
+        console.error("删除数据时出错:", chrome.runtime.lastError);
+      } else {
+        console.log("清理缓存:", keysToRemove);
+      }
+    });
+  } else {
+    console.log("没有找到以 '_page' 结尾的键名对应的项");
+  }
+}
 
 
 
